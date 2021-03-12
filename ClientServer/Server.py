f71eb56d562a41a94497bd5e36d8ebff20b6d2ed
Hemera-ats <51821803+Hemera-ats@users.noreply.github.com>
2021-03-12 23:09:04 +0700

    Add files via upload

---
+++ ClientServer/Server.py
@@ -0,0 +1,60 @@
+import socket #ip + port number pair, ip identifies hist
+import select
+from threading import Thread # use to allow multiple pieces of code to run concurrently
+import os
+import argparse
+
+Server_host = "127.0.0.1"
+Server_port = 8080
+separator_token = "<SEP>" # we will use this to separate the client name & message
+     
+client_sockets = set()
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #True, reconnect
+
+serverSocket.bind((Server_host, Server_port))
+
+serverSocket.listen(5)
+print(f"[*] Listening as {Server_host}:{Server_port}")
+
+def listen_for_client(cs):
+    """
+    This function keep listening for a message from `cs` socket
+    Whenever a message is received, broadcast it to all other connected clients
+    """
+    while True:
+        try:
+            # keep listening for a message from `cs` socket
+            msg = cs.recv(1024).decode()
+        except Exception as e:
+            # client no longer connected
+            # remove it from the set
+            print(f"[!] Error: {e}")
+            client_sockets.remove(cs)
+        else:
+            # if we received a message, replace the <SEP> 
+            # token with ": " for nice printing
+            msg = msg.replace(separator_token, ": ")
+        # iterate over all connected sockets
+        for client_socket in client_sockets:
+            # and send the message
+            client_socket.send(msg.encode())
+
+while True:
+    # we keep listening for new connections all the time
+    client_socket, client_address = serverSocket.accept()
+    print(f"[+] {client_address} connected.")
+    # add the new connected client to connected sockets
+    client_sockets.add(client_socket)
+    # start a new thread that listens for each client's messages
+    t = Thread(target=listen_for_client, args=(client_socket,))
+    # make the thread daemon so it ends whenever the main thread ends
+    t.daemon = True
+    # start the thread
+    t.start()
+
+# close client sockets
+for cs in client_sockets:
+    cs.close()
+# close server socket
+serverSocket.close()
